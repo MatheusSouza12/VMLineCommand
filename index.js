3dc19206cc85e24840d0847db4ed4a268d302d2c
Matheus <matheus007junior@hotmail.com>
2019-06-21 20:00:38 -0300

    Conseguindo conexao com o front e criando a vm

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var http = require("http"); //Requisitando a biblioteca http
 var shell = require("shelljs");
+const url = require('url');
 
-http
-  .createServer(function(req, res) {
+const server = http
+  .createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.writeHead(200, {
       "Content-Type": "text/plain; charset=utf-8"
     });
-    res.write("Olá mundo!");
-    res.end();
-  })
-  .listen(3000);
+    if (req.method === 'GET') {
+      const {
+        query: {
+          name,
+          cpu,
+          ram,
+          ip
+        }
+      } = url.parse(req.url, true);
+      shell.echo(name)
+      shell.echo(cpu)
+      shell.echo(ram)
+      shell.echo(ip)
+      shell.exec(
+        `vboxmanage createvm --name ${name} --ostype Ubuntu_64 --register;\n` +
+        `vboxmanage modifyvm ${name} --memory ${ram};\n`
+      );
+    }
+    return res.end();
+  });
+server.listen(3000, () => console.log('Listening on port 3000'));
 
-shell.echo("hello world");
-shell.exec("mkdir teste");
-shell.exec(
-  "vboxmanage createvm --name LinhaDeComando --ostype Ubuntu_64 --register"
-);
+
 console.log("Servidor iniciado!");
